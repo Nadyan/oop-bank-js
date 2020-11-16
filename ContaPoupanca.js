2238246d6ce04e6a6cef768fb479b6c82bce43da
Nadyan <nadyan_sp@hotmail.com>
2020-11-16 19:55:41 -0300

    cp update

--- ContaPoupanca.js
+++ ContaPoupanca.js
@@ -5,5 +5,10 @@ export class ContaPoupanca extends Conta {
     constructor(agencia, numero, variacao, cliente) {
         super(agencia, numero, cliente);
         this._variacao = variacao;
+        this._percJuros = 0.05; // 5%
+    }
+
+    calculaJuros() {
+        return this._saldo * this._percJuros;
     }
 }
--- index.js
+++ index.js
@@ -8,3 +8,6 @@ const cp = new ContaPoupanca(1394, 29244, 51, cliente);
 
 console.log(cc);
 console.log(cp);
+
+cp.depositar(100);
+console.log(cp.calculaJuros());
