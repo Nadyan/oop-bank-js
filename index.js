63fe1b3c24f8c337886de7acb8a06935d7149344
Nadyan <nadyan_sp@hotmail.com>
2020-11-16 22:05:23 -0300

    abstract class and method

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import { Cliente } from "./Cliente.js"
 import { ContaCorrente } from "./ContaCorrente.js"
 import { ContaPoupanca } from "./ContaPoupanca.js";
 
-const cliente = new Cliente("Nadyan", "04994967954");
-const cc = new ContaCorrente(1394, 29244, cliente);
-const cp = new ContaPoupanca(1394, 29244, 51, cliente);
+const cliente = new Cliente("Nadyan", "04988907454");
+const cc = new ContaCorrente(1491, 22234, cliente);
+const cp = new ContaPoupanca(1491, 22234, 51, cliente);
 
 //console.log(cc);
 //console.log(cp);
